3031b84528e23544cf0d0d22be07292e6c9303cf
KatieWP <kwareham.dev@gmail.com>
2023-05-27 16:31:24 +0100

    Commented code

--- src/main.rs
+++ src/main.rs
@@ -6,24 +6,28 @@ const START: usize = 0;
 const END: usize = 100;
 fn main() {
     // Read file data
-
     let file = File::open("src/data/challenge_input.txt").unwrap();
-    // let file = File::open("src/data/challange_5.txt").unwrap();
+    //let file = File::open("src/data/challenge_5.txt").unwrap();
     let v: Vec<i32> = BufReader::new(file)
         .lines()
         .flatten() // gets rid of Err from lines
         .flat_map(|line| line.parse::<i32>()) // ignores Err variant from Result of str.parse
         .collect();
 
+    // Hashmap where the key and value are stored:
+    // key = is the sum of the values
+    // value = is the amount of times the result of adding two numbers is repeated.
     let mut map_sum: HashMap<i32, i32> = HashMap::new();
     map_sum = start_iter_over_map(v.clone(), map_sum.clone());
 
-    // loop somehow to all the values in the file and add to start and end depending on function to do
+    // loop through to all the values in the file (after the 100th position) and check whether its a valid value.
+    // Then proceed to remove from the hashmap (or -1 value) all the keys that resulted form the sum of the first number to the rest in the list.
+    // After we will add the values of the checked_value + the list of the previous 99 values. This will result in the sum of previous 100 values of the next number to check in the list.
     let mut start = START;
     let mut end = END;
     'find: loop {
         let check_value = v[end];
-        // println!("\nmap_sum: {:?}", map_sum);
+        // Break if the value is not secure
         if check_next_val(map_sum.clone(), &check_value) == false {
             println!(
                 "Value ({:?}) not secure. Position: {}",
@@ -33,19 +37,19 @@ fn main() {
             break 'find;
         } else {
             map_sum = remove_key_value(v.clone(), map_sum.clone(), start, end);
-            // println!("after remove: {:?}", map_sum);
             start += 1;
             map_sum = add_key_value(v.clone(), map_sum, start, end);
-            // println!("after add: {:?}", map_sum);
             end += 1;
         }
 
+        //Break if we go over the content length of the file.
         if end >= v.len() {
             break 'find;
         }
     }
 }
 
+// Get first iteration of the first 100 elements and add each value to the hashmap
 fn start_iter_over_map(content: Vec<i32>, mut new_map: HashMap<i32, i32>) -> HashMap<i32, i32> {
     for x in START..END - 1 {
         for y in (x + 1)..END {
@@ -59,32 +63,20 @@ fn start_iter_over_map(content: Vec<i32>, mut new_map: HashMap<i32, i32>) -> Has
     new_map
 }
 
+// checks if the value is a key in the hashmap.
 fn check_next_val(map_sum: HashMap<i32, i32>, val: &i32) -> bool {
-    // println!("Checking value... {}", val);
     map_sum.contains_key(val)
 }
 
+// remove all the sums that the first value in the list has with the rest of the values.
+// Return the remaining hashmap.
 fn remove_key_value(
     content: Vec<i32>,
     mut map_sum: HashMap<i32, i32>,
     start: usize,
     end: usize,
 ) -> HashMap<i32, i32> {
-    // println!("\nRemoving value: {}", content[start]);
-    // println!(
-    //     "From {} to {}  ({}, {})",
-    //     content[start + 1],
-    //     content[end - 1],
-    //     start + 1,
-    //     end - 1
-    // );
     for y in (start + 1)..(end) {
-        // println!(
-        //     "Removing sum of {} + {} = {}",
-        //     content[start],
-        //     content[y],
-        //     (content[start] + content[y])
-        // );
         if let Some(value) = map_sum.get_mut(&(content[start] + content[y])) {
             *value -= 1;
             if *value == 0 {
@@ -95,27 +87,15 @@ fn remove_key_value(
     map_sum
 }
 
+// Addd the new values that result in the sum of the end value with the rest of the numbers in the list.
+// return the changed hashmap
 fn add_key_value(
     content: Vec<i32>,
     mut map_sum: HashMap<i32, i32>,
     new_start: usize,
     new_end: usize,
 ) -> HashMap<i32, i32> {
-    // println!("\nAdding value: {}", content[new_end]);
-    // println!(
-    //     "From {} to {} ({}, {})",
-    //     content[new_start],
-    //     content[new_end - 1],
-    //     new_start,
-    //     new_end - 1
-    // );
     for x in new_start..(new_end) {
-        // println!(
-        //     "Adding sum of {} + {} = {} ",
-        //     content[new_end],
-        //     content[x],
-        //     (content[x] + content[new_end])
-        // );
         if let Some(value) = map_sum.get_mut(&(content[x] + content[new_end])) {
             *value += 1;
         } else {
